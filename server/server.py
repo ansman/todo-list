8154466acbe4c193b5e3d567440171000f239e1c
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-12-18 23:41:37 +0100

    Allow customizing the api server port

--- server/server.py
+++ server/server.py
@@ -1,5 +1,11 @@
+import sys
+
 from sudo import config, app, setup_api
 
 if __name__ == '__main__':
     setup_api(app)
-    app.run(debug=app.config['DEBUG'])
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 4000
+    app.run(debug=app.config['DEBUG'], port=port)
