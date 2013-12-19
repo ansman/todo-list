5a30a10909b456563ade67d8d707005cc3602385
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-12-19 19:22:19 +0100

    Disable caching of API responses

--- server/sudo/__init__.py
+++ server/sudo/__init__.py
@@ -9,6 +9,7 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, HEAD'
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Cache-Control'] = 'No-Cache'
     return response
 
 def setup_api(app):
