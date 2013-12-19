733eb3d4329fe6a5613f8364cfae3161a9ea59b9
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-12-20 00:38:01 +0100

    Update the name of the database env variable

--- server/sudo/config.py
+++ server/sudo/config.py
@@ -2,7 +2,7 @@ import os
 from urlparse import urlparse
 
 def load_config(app):
-    uri = os.environ.get("DATABASE", "postgresql://sudo:p4ssw0rd@localhost:5432/sudo")
+    uri = os.environ.get("HEROKU_POSTGRESQL_WHITE_URL", "postgresql://sudo:p4ssw0rd@localhost:5432/sudo")
     uri = urlparse(uri)
 
     app.config.update(DATABASE_HOST=uri.hostname,
