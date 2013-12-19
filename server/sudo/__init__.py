d12d84ad43e548cb71d72ff31759098ef3447d07
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-12-19 14:51:22 +0100

    Correctly add CORS headers to all requests

--- server/sudo/__init__.py
+++ server/sudo/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
-from sudo import config, db, routes, response
+from sudo import config, db, routes
 
 app = Flask(__name__)
-app.response_class = response.Response
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, HEAD'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 def setup_api(app):
     config.load_config(app)
