e57659ce99176029881fc3185d32f2db8424eef7
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-12-20 01:43:33 +0100

    Add a test for markAllAsComplete

--- client/specs/collections/todos-spec.js
+++ client/specs/collections/todos-spec.js
@@ -21,5 +21,28 @@ define('specs/collections/todos-spec.js', ['collections/todos'], function(Todos)
         expect(todos.todosLeft()).toBe(3);
       });
     });
+
+    describe("markAllAsComplete", function() {
+      it("marks all todos as complete", function() {
+        var collection = new Todos([{
+          id: 1,
+          completed: false
+        }, {
+          id: 2,
+          completed: true
+        }, {
+          id: 3,
+          completed: false
+        }, {
+          id: 4,
+          completed: false
+        }]);
+
+        spyOn(collection, 'sync');
+        collection.markAllAsComplete();
+        expect(collection.where({completed: false})).toEqual([]);
+        expect(collection.sync).toHaveBeenCalled();
+      });
+    });
   });
 });
